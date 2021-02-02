c8c4f5ab6ba5fb3c7b6f83042924fec6b4fe6316
Philip Schlump <pschlump@gmail.com>
2021-02-02 09:48:57 -0700

    Initial checkin of code for assignent

---
+++ bsvr/merkle/merkle.go
@@ -0,0 +1,35 @@
+package merkle
+
+import (
+	"github.com/Univ-Wyo-Education/S20-4010-a03/bsvr/hash"
+)
+
+func MerkleHash(data [][]byte) []byte {
+	// Build a place to put the hashes for the leaves
+	hLeaf := make([][]byte, 0, len(data))
+	// Calculate Leaf Hashes
+	for ii := range data {
+		aHash := hash.HashOf(data[ii])
+		hLeaf = append(hLeaf, aHash)
+	}
+	ln := (len(hLeaf) / 2) + (len(hLeaf) % 2)
+	hMid := make([][]byte, 0, ln)
+	for ln > 0 {
+		for ii := 0; ii < len(hLeaf); ii += 2 {
+			if ii+1 < len(hLeaf) {
+				hT := hash.Keccak256(hLeaf[ii], hLeaf[ii+1])
+				hMid = append(hMid, hT)
+			} else {
+				hT := hash.Keccak256(hLeaf[ii])
+				hMid = append(hMid, hT)
+			}
+		}
+		hLeaf = hMid
+		ln = (len(hLeaf) / 2) + (len(hLeaf) % 2)
+		hMid = make([][]byte, 0, ln)
+		if len(hLeaf) == 1 {
+			return hLeaf[0]
+		}
+	}
+	return hLeaf[0]
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Univ-Wyo-Education/S21-4010-a03
+
+go 1.15
+
+require github.com/pschlump/godebug v1.0.1
